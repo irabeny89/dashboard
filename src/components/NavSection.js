f69efa8168816874825d23c75ae369da7c2772ef
Ernest Irabor <irabeny89@gmail.com>
2021-01-07 23:23:38 +0100

    Partial styling

--- src/components/NavSection.js
+++ src/components/NavSection.js
@@ -3,20 +3,29 @@ import styles from '../../styles/NavSection.module.css'
 import { FaFemale, FaMale, FaSearch, FaUsers } from 'react-icons/fa'
 
 const NavSection = ({user="user"}) => 
-  <div className={styles.navsection}>
+  <div className={styles.navSection}>
     <h1><span>Hello</span>, {user}</h1>
-    <p>Welcome to your dashboard, kindly sort through the user base</p>
-    <div className={styles.inputsearch}><FaSearch /><input placeholder="Find a user" /></div>
+    <p className={styles.welcome}>
+      Welcome to your dashboard, kindly sort through the user base
+    </p>
+    <div className={styles.inputSearch}>
+      <FaSearch size="25" /><input placeholder="Find a user" />
+    </div>
     <h4>Show Users</h4>
-    <div>
+    <div className={styles.options}>
       <div>
-        <FaUsers /><p>All Users</p>
+        <div className={styles.allUsersIcon}><FaUsers size="50" /></div>
+        <p>All Users</p>
       </div>
       <div>
-        <FaMale /><p>Male Users</p>
+        <div className={styles.allMaleUsersIcon}><FaMale size="50" /></div>
+        <p>Male Users</p>
       </div>
       <div>
-        <FaFemale /><p>Female Users</p>
+        <div className={styles.allFemaleUsersIcon}>
+          <FaFemale size="50" />
+        </div>
+        <p>Female Users</p>
       </div>
     </div>
   </div>
