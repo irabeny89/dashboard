3e3ce62fbe043d3e2cab1b09787fb90a5e3b2369
Ernest Irabor <irabeny89@gmail.com>
2021-01-07 19:17:35 +0100

    SEO optimisation

---
+++ pages/_document.js
@@ -0,0 +1,21 @@
+import Document, {
+  Html, Head, Main, NextScript
+} from 'next/document'
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <meta name="dashboard" content="User dashboard" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
