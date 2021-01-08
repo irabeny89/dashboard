900a4bb80e8671b9b8e4fb01e2c141c6edb12c81
Ernest Irabor <irabeny89@gmail.com>
2021-01-08 16:38:15 +0100

    ✨ Context wrapper

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,10 @@
 import '../styles/globals.css'
+import OutputSectionProvider from '../src/components/OutputSectionProvider'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <OutputSectionProvider><Component {...pageProps} /></OutputSectionProvider>
+  )
 }
 
 export default MyApp
