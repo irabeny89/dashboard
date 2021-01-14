318f6be315e24e2b62ffc301e82954e40f74b6cf
Ernest Irabor <irabeny89@gmail.com>
2021-01-14 11:00:11 +0100

    💄 🎨 Improve code format and UI styles

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -9,7 +9,7 @@ const Profile = ({ user }) => {
   const { setShowProfile } = useContext(Context)
   const profile = user[0]
   const {
-    picture: { thumbnail },
+    picture: { large },
     name: { title, first, last },
     dob: { age },
     location: { street: { number, name }, city, state },
@@ -24,19 +24,20 @@ const Profile = ({ user }) => {
       <div onClick={() => setShowProfile(false)} className={styles.back}>
         <CgArrowLeft color="#30bbb5" size="35" /> &nbsp;&nbsp;RESULTS
       </div>
-      <div className={styles.imageDiv}>
-        <Image className={styles.image} src={thumbnail} width="200"
-          height="200" />
+      <div className={styles.details}>
+        <div className={styles.imageDiv}>
+          <Image className={styles.image} src={large} width="200"
+            height="200" />
+        </div>
+        <div>
+          <h2>{title}. {first} {last} <span>{age}</span></h2>
+          <p>{number} {name}, {city}, {state}</p>
+          <p className={styles.email}><CgMail /> {email}</p>
+          <p className={styles.joined}>JOINED: {registered.date.substr(0, 10)}</p>
+          <p><FiPhoneCall /> {phone}</p>
+          <p><FiSmartphone /> {cell}</p>
+        </div>
       </div>
-      <div>
-        <h2>{title}. {first} {last} <span>{age}</span></h2>
-        <p>{number} {name}, {city}, {state}</p>
-        <p className={styles.email}><CgMail /> {email}</p>
-        <p className={styles.joined}>JOINED: {registered.date.substr(0, 10)}</p>
-        <p><FiPhoneCall /> {phone}</p>
-        <p><FiSmartphone /> {cell}</p>
-      </div>
-      
     </div>
   )
 }
